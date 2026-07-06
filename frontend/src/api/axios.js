7c85c0a2b7d2f9c4b763e1e13bd25669159d7afc
Milagros Acevedo <mili.acevedo99@gmail.com>
2026-07-06 20:18:25 -0300

    Update backend URL to Render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://mate-travel-backend.onrender.com/api',
 });
 
 // Interceptor para inyectar el token en cada petición
